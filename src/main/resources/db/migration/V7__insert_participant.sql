56a8bcc0531fc34dfdbfd5c72add87c29c37e337
freddy24-7 <freddy.austli@aol.com>
2023-05-01 23:27:01 +0200

    minor adjustment

--- src/main/resources/db/migration/V7__insert_participant.sql
+++ src/main/resources/db/migration/V7__insert_participant.sql
@@ -1,3 +1,3 @@
 INSERT INTO participant (id, email, first_name, last_name, mobile_number, photoURL, postcode, user_id)
-VALUES (15, 'testuser15@gmail.com', 'finn', 'bakker', '0617454545', 'www.photo.com', '3543KK', 15)
+VALUES (15, 'testuser15@gmail.com', 'finn', 'bakker', '0617455834', 'www.photo.com', '3543HZ', 15)
 
